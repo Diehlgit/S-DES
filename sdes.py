43189b0ce8abdc16c1f634298e9b06f00718f60e
Arthur Diehl Barroso <arthurdiehlbarroso@gmail.com>
2025-05-05 11:15:57 -0300

    Added definition to P10() function.

--- sdes.py
+++ sdes.py
@@ -11,7 +11,17 @@
 #def SW():
 
 #Key Permutation Function 
-#def P10():
+def P10(key: int):
+    if not (0 <= key < 1024):
+        raise ValueError("Input must br a 10-bit integer")
+
+    bin_str = f"{key:010b}"
+
+    p10_idx = [2, 4, 1, 6, 3, 9, 0, 8, 7, 5]
+
+    permutation = ''.join(bin_str[i] for i in p10_idx)
+
+    return int(permutation, 2)
 
 #Permutation Function that produces subkeys (K1 and K2)
 #def P8():
